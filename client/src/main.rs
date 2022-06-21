a2dc610e92520c7a5ed52bd7952d91a6eb0eb6be
Veritius <veritiusgaming@gmail.com>
2022-06-21 12:24:51 +1000

    Stuff

--- client/src/main.rs
+++ client/src/main.rs
@@ -13,47 +13,47 @@ mod resources;
 fn main() {
     info!("Starting client");
 
-    App::new()
-        // Game essentials
-        .add_plugins(DefaultPlugins)
-        .insert_resource(WindowDescriptor {
-            title: "Rustcraft vUnknown".to_string(),
-            width: 500.,
-            height: 300.,
-            present_mode: PresentMode::Fifo,
-            ..default()
-        })
-        .add_plugin(PhysicsPlugin::default())
+    let mut app = App::new();
+    // Game essentials
+    app.add_plugins(DefaultPlugins);
+    app.insert_resource(WindowDescriptor {
+        title: "Rustcraft vUnknown".to_string(),
+        width: 500.,
+        height: 300.,
+        present_mode: PresentMode::Fifo,
+        ..default()
+    });
+    app.add_plugin(PhysicsPlugin::default());
+    
+    // Networking
+    app.add_plugin(ClientPlugin::<Protocol, Channels>::new(
+        ClientConfig::default(),
+        shared_config(),
+    ));
+    
+    // Startup System
+    app.add_startup_system(systems::init);
+    // Realtime Gameplay Loop
+    app.add_system_to_stage(Stage::Connection, events::connect_event);
+    app.add_system_to_stage(Stage::Disconnection, events::disconnect_event);
+    app.add_system_to_stage(Stage::ReceiveEvents, events::spawn_entity_event);
+    app.add_system_to_stage(Stage::ReceiveEvents, events::insert_component_event);
+    app.add_system_to_stage(Stage::ReceiveEvents, events::update_component_event);
+    app.add_system_to_stage(Stage::ReceiveEvents, events::receive_message_event);
+    app.add_system_to_stage(Stage::Frame, input);
+    app.add_system_to_stage(Stage::PostFrame, sync);
+    // Gameplay Loop on Tick
+    app.add_system_to_stage(Stage::Tick, tick);
         
-        // Networking
-        .add_plugin(ClientPlugin::<Protocol, Channels>::new(
-            ClientConfig::default(),
-            shared_config(),
-        ))
-        
-        // Startup System
-        .add_startup_system(systems::init)
-        // Realtime Gameplay Loop
-        .add_system_to_stage(Stage::Connection, events::connect_event)
-        .add_system_to_stage(Stage::Disconnection, events::disconnect_event)
-        .add_system_to_stage(Stage::ReceiveEvents, events::spawn_entity_event)
-        .add_system_to_stage(Stage::ReceiveEvents, events::insert_component_event)
-        .add_system_to_stage(Stage::ReceiveEvents, events::update_component_event)
-        .add_system_to_stage(Stage::ReceiveEvents, events::receive_message_event)
-        .add_system_to_stage(Stage::Frame, input)
-        .add_system_to_stage(Stage::PostFrame, sync)
-        // Gameplay Loop on Tick
-        .add_system_to_stage(Stage::Tick, tick)
-        
-        // Console
-        .add_plugin(ConsolePlugin)
+    // Console
+    app.add_plugin(ConsolePlugin);
 
-        // Discord Rich Presence
-        .add_plugin(RPCPlugin(RPCConfig {
-            app_id: 987633651728666645,
-            show_time: true
-        }))
+    // Discord Rich Presence
+    app.add_plugin(RPCPlugin(RPCConfig {
+        app_id: 987633651728666645,
+        show_time: true
+    }));
         
-        // Start the game
-        .run();
+    // Start the game
+    app.run();
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,12 +1,16 @@
 use bevy_app::{App, ScheduleRunnerPlugin};
 use bevy_core::CorePlugin;
+use bevy_ecs::system::Command;
 use bevy_log::{info, LogPlugin};
 use bevy_math::f32::Vec3;
+use bevy_ecs::prelude::Commands;
 use heron::prelude::*;
 
 use naia_bevy_server::{Plugin as ServerPlugin, ServerConfig, Stage};
+use naia_shared::Property;
 
-use rustcraft_shared::{protocol::Protocol, shared_config, Channels};
+use rustcraft_shared::{protocol::GridFixture};
+use rustcraft_shared::{protocol::Protocol, protocol::Position, shared_config, Channels};
 
 mod resources;
 mod systems;
@@ -16,28 +20,31 @@ use systems::{events, init, tick};
 fn main() {
     info!("Starting game server");
 
-    // Build App
-    App::new()
-        // Game essentials
-        .add_plugin(CorePlugin::default())
-        .add_plugin(ScheduleRunnerPlugin::default())
-        .add_plugin(LogPlugin::default())
-        .add_plugin(ServerPlugin::<Protocol, Channels>::new(
-            ServerConfig::default(),
-            shared_config(),
-        ))
+    // Make app
+    let mut app = App::new();
 
-        // Physics
-        .add_plugin(PhysicsPlugin::default())
-        .insert_resource(Gravity::from(Vec3::new(0.0, -9.81, 0.0)))
+    // Game essentials
+    app.add_plugin(CorePlugin::default());
+    app.add_plugin(ScheduleRunnerPlugin::default());
+    app.add_plugin(LogPlugin::default());
 
-        .add_startup_system(init)
-        .add_system_to_stage(Stage::ReceiveEvents, events::authorization_event)
-        .add_system_to_stage(Stage::ReceiveEvents, events::connection_event)
-        .add_system_to_stage(Stage::ReceiveEvents, events::disconnection_event)
-        .add_system_to_stage(Stage::ReceiveEvents, events::receive_message_event)
-        .add_system_to_stage(Stage::Tick, tick)
 
-        // Start the game
-        .run();
+    // Physics
+    app.add_plugin(PhysicsPlugin::default());
+    app.insert_resource(Gravity::from(Vec3::new(0.0, -9.81, 0.0)));
+
+    // Networking
+    app.add_plugin(ServerPlugin::<Protocol, Channels>::new(
+        ServerConfig::default(),
+        shared_config(),
+    ));
+    app.add_startup_system(init);
+    app.add_system_to_stage(Stage::ReceiveEvents, events::authorization_event);
+    app.add_system_to_stage(Stage::ReceiveEvents, events::connection_event);
+    app.add_system_to_stage(Stage::ReceiveEvents, events::disconnection_event);
+    app.add_system_to_stage(Stage::ReceiveEvents, events::receive_message_event);
+    app.add_system_to_stage(Stage::Tick, tick);
+
+    // Start the game
+    app.run();
 }
