be56302a9ddda82159e2ed2cc55019116d6e9f6b
Veritius <veritiusgaming@gmail.com>
2022-07-18 10:05:13 +1000

    Even more re-exports

--- client/src/systems/events.rs
+++ client/src/systems/events.rs
@@ -35,7 +35,7 @@ pub fn disconnect_event(client: Client<Protocol, Channels>) {
     info!("Client disconnected from: {}", client.server_address());
 }
 
-pub fn spawn_entity_event(mut event_reader: EventReader<SpawnEntityEvent>) {
+pub fn spawn_entity_event(mut event_reader  : EventReader<SpawnEntityEvent>) {
     for event in event_reader.iter() {
         match event {
             SpawnEntityEvent(_entity) => {
