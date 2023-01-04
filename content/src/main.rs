289ff593ee4bd01c20612e409cdc3a54022714c9
Veritius <veritiusgaming@gmail.com>
2023-01-04 23:29:12 +1100

    App

--- content/src/main.rs
+++ content/src/main.rs
@@ -1,5 +1,17 @@
+use bevy::{prelude::App, DefaultPlugins};
+use blocks::{Dirt, Stone};
+use rustcraft_modlib::{BlockRegistryPlugin, block::traits::AddBlock};
+
 pub mod blocks;
 
 fn main() {
-    println!("Hello, world!");
+    let mut app = App::new();
+
+    app.add_plugins(DefaultPlugins);
+    app.add_plugin(BlockRegistryPlugin);
+
+    app.add_block::<Dirt>();
+    app.add_block::<Stone>();
+
+    app.run();
 }
