2535c55803d5b7ca33d8568e8ae03cd49a5fb307
Veritius <veritiusgaming@gmail.com>
2023-01-05 17:07:47 +1100

    New setters and getters

--- modlib/src/chunk/resource.rs
+++ modlib/src/chunk/resource.rs
@@ -1,29 +1,20 @@
 use std::collections::BTreeMap;
 use bevy::prelude::{Resource, Entity};
 
-use super::Chunk;
-
 type ChunkCoordinate = (u32, u32, u32);
 
 #[derive(Resource)]
 pub struct ChunkRegistry {
-    registry: BTreeMap<u32, BTreeMap<u32, BTreeMap<u32, Option<Entity>>>>,
+    registry: BTreeMap<u32, BTreeMap<u32, BTreeMap<u32, Entity>>>,
 }
 
 impl ChunkRegistry {
-    pub fn get(&self, coord: ChunkCoordinate) -> Result<&Option<Entity>, ChunkOperationError> {
+    pub fn get(&self, coord: ChunkCoordinate) -> Result<Option<&Entity>, ChunkOperationError> {
         match self.registry.get(&coord.0) {
             Some(one) => {
                 match one.get(&coord.1) {
                     Some(two) => {
-                        match two.get(&coord.2) {
-                            Some(chunk) => {
-                                return Ok(chunk)
-                            },
-                            None => {
-                                return Err(ChunkOperationError::NoChunkInPosition(coord))
-                            },
-                        }
+                        return Ok(two.get(&coord.2))
                     },
                     None => {
                         return Err(ChunkOperationError::NoChunkInPosition(coord))
@@ -34,18 +25,22 @@ impl ChunkRegistry {
         }
     }
 
-    fn get_mut(&mut self, coord: ChunkCoordinate) -> Result<&mut Option<Entity>, ChunkOperationError> {
+    pub fn set(&mut self, coord: ChunkCoordinate, to: Option<Entity>) -> Result<(), ChunkOperationError> {
         match self.registry.get_mut(&coord.0) {
             Some(one) => {
                 match one.get_mut(&coord.1) {
                     Some(two) => {
-                        match two.get_mut(&coord.2) {
-                            Some(chunk) => {
-                                return Ok(chunk)
+                        match to {
+                            Some(value) => {
+                                match two.get(&coord.2) {
+                                    Some(_) => { return Err(ChunkOperationError::ChunkAlreadyPresent(coord)) },
+                                    None => {
+                                        two.insert(coord.2, value);
+                                        return Ok(())
+                                    },
+                                }
                             },
-                            None => {
-                                return Err(ChunkOperationError::NoChunkInPosition(coord))
-                            },
+                            None => todo!(),
                         }
                     },
                     None => {
@@ -56,22 +51,9 @@ impl ChunkRegistry {
             None => { return Err(ChunkOperationError::NoChunkInPosition(coord)) },
         }
     }
-
-    pub fn assign_chunk(&mut self, coord: ChunkCoordinate, to: Option<Entity>) -> Result<(), ChunkOperationError> {
-        match self.get_mut(coord) {
-            Ok(value) => {
-                *value = to;
-                return Ok(())
-            },
-            Err(error) => { return Err(error) },
-        }
-    }
-
-    pub fn clear_chunk(&mut self, coord: ChunkCoordinate) -> Result<(), ChunkOperationError> {
-        self.assign_chunk(coord, None)
-    }
 }
 
 pub enum ChunkOperationError {
-    NoChunkInPosition(ChunkCoordinate)
+    NoChunkInPosition(ChunkCoordinate),
+    ChunkAlreadyPresent(ChunkCoordinate),
 }
