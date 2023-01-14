8f841413c2e2bf37b7b65c8600313503835dcfba
Veritius <veritiusgaming@gmail.com>
2023-01-14 13:03:13 +1100

    Tweaks

--- modlib/src/world/block/entity.rs
+++ modlib/src/world/block/entity.rs
@@ -1,7 +1,13 @@
 use bevy::prelude::Component;
 use crate::world::chunk::meshing::MeshingVisibility;
 
+use super::BlockId;
+
+// Block entities store a
 #[derive(Component)]
-pub struct BlockEntity {
-    pub visibility: MeshingVisibility,
+pub enum BlockEntity {
+    Custom {
+        visibility: MeshingVisibility,
+    },
+    Generic(BlockId),
 }
