58bf6fb259d07649dd8a2ac513efef80ef04dea5
Veritius <veritiusgaming@gmail.com>
2023-01-24 23:32:32 +1100

    Tweaks to registry traits

--- modlib/src/registries/mod.rs
+++ modlib/src/registries/mod.rs
@@ -5,18 +5,22 @@ pub mod startup_write;
 
 use startup_write::{StartupWriteResource, StartupWriteInternal, StartupWriteBuffer};
 
-pub trait Registry<B: RegistryBuffer<I, E>, I: RegistryInternal<E>, E: RegistryEntry>: StartupWriteResource<B, I> {
-
+pub trait Registry<B: RegistryBuffer<I, E, Id>, I: RegistryInternal<E, Id>, E: RegistryEntry, Id: RegistryIdentifier>: StartupWriteResource<B, I> {
+    fn get_entry(&self) -> &E;
 }
 
-pub trait RegistryBuffer<I: RegistryInternal<E>, E: RegistryEntry>: StartupWriteBuffer<I> {
-
+pub trait RegistryBuffer<I: RegistryInternal<E, Id>, E: RegistryEntry, Id: RegistryIdentifier>: StartupWriteBuffer<I> {
+    fn add_entry(&mut self, entry: E);
 }
 
-pub trait RegistryInternal<E: RegistryEntry>: StartupWriteInternal {
-
+pub trait RegistryInternal<E: RegistryEntry, Id: RegistryIdentifier>: StartupWriteInternal {
+    fn get_entry(&self, id: Id) -> &E;
 }
 
 pub trait RegistryEntry {
+    
+}
+
+pub trait RegistryIdentifier: PartialEq + Eq {
 
 }
