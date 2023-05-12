e9f488d39b74c7d7b83f8103e68a03b7349a89c4
Veritius <veritiusgaming@gmail.com>
2023-05-12 15:42:47 +1000

    Save changes

--- modlib/src/world/render/blockatlas.rs
+++ modlib/src/world/render/blockatlas.rs
@@ -1,41 +1,22 @@
 use std::{sync::{Arc, RwLock}, collections::BTreeMap};
 use bevy::{render::once_cell::sync::Lazy, prelude::{Handle, Image, Assets}, math::Rect};
+use serde::{Serialize, Deserialize};
 
-pub(crate) static BLOCK_ATLAS_TEXTURE: Lazy<Arc<Option<RwLock<DynamicAtlasTexture>>>> = Lazy::new(|| Arc::new(None));
+pub(crate) static BLOCK_ATLAS_TEXTURE: Lazy<Arc<RwLock<Option<BlockAtlas>>>> = Lazy::new(||Arc::new(RwLock::new(None)));
 
-/// Special implementation of texture atlases, similar to Bevy's, with some modifications.
-/// 
-/// - Data is accessible outside a tick.
-///     - This is useful for asynchronous applications.
-///     - Images can only be added to the atlas texture within a tick.
-/// - Associated string rather than number
-/// - Add textures at any point
-///     - You can add textures at any point, and it'll repack.
-///     - This will very likely break UVs.
-pub struct DynamicAtlasTexture {
-    pub handle: Handle<Image>,
-    pub rects: BTreeMap<String, Rect>,
+pub(super) fn block_atlas_creation_system(
+
+) {
+
 }
 
-impl DynamicAtlasTexture {
-    /// Adds textures to the atlas. This will always repack the image atlas, be careful!
-    pub fn add_textures(&mut self, assets: &mut Assets<Image>, images: Vec<(String, Image)>) {
-        // Take ownership of images for rect extraction.
-        let mut images = images;
+pub struct BlockAtlas(pub BTreeMap<String, Rect>);
 
-        // Get image asset associated with this atlas texture.
-        let image_asset = assets.get_mut(&self.handle).expect("Atlas texture handle should have been valid!");
+#[derive(Serialize, Deserialize)]
+struct BlockTextures(Vec<BlockTexture>);
 
-        // Extract existing rectangles.
-        for rect in &self.rects {
-            
-        }
-
-        // Delete everything in rects.
-        self.rects.clear();
-    }
-
-    pub fn get_texture_uvs(&self, name: String) -> Rect {
-        todo!();
-    }
+#[derive(Serialize, Deserialize)]
+struct BlockTexture {
+    name: String,
+    pos: [u16;2],
 }
--- modlib/src/world/render/mod.rs
+++ modlib/src/world/render/mod.rs
@@ -7,6 +7,7 @@ use self::shader::RepeatingTextureMaterial;
 pub struct BlockShadersPlugin;
 impl Plugin for BlockShadersPlugin {
     fn build(&self, app: &mut App) {
+        app.add_startup_system(blockatlas::block_atlas_creation_system);
         app.add_plugin(MaterialPlugin::<RepeatingTextureMaterial>::default());
     }
 }
