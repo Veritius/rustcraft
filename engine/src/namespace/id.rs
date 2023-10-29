752fa3199dcf462bbd104d2a80f34220dc456faa
Veritius <veritiusgaming@gmail.com>
2023-10-29 21:09:26 +1100

    Remove TypeId variant

--- engine/src/namespace/id.rs
+++ engine/src/namespace/id.rs
@@ -2,7 +2,6 @@ use std::any::TypeId;
 
 /// An identifier object for a content package or a piece of content.
 pub enum ContentIdentifier {
-    TypeId(TypeId),
     String(String),
     Integer(u64),
 }
