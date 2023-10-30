d3cfc46f9f1270924fe3ea2ec24b091e939d51fb
veritius <veritiusgaming@gmail.com>
2023-10-30 14:03:48 +1100

    Save changes

--- engine/src/content/package.rs
+++ engine/src/content/package.rs
@@ -1,13 +1,34 @@
-use mlua::FromLua;
+use mlua::{FromLua, IntoLua};
+use semver::{Version, VersionReq};
 use super::id::Identifier;
 
+/// A content package.
+/// 
+/// Equality (`PartialEq`/`Eq`) only compares the `identifier` and `version` fields.
+#[derive(Debug)]
 pub struct ContentPackage {
     pub identifier: Identifier,
+    pub version: Version,    
+    pub dependencies: Vec<(Identifier, VersionReq)>,
     pub owners: Vec<String>,
 }
 
+impl PartialEq for ContentPackage {
+    fn eq(&self, other: &Self) -> bool {
+        self.identifier == other.identifier && self.version == other.version
+    }
+}
+
+impl Eq for ContentPackage {}
+
 impl FromLua<'_> for ContentPackage {
     fn from_lua(value: mlua::Value<'_>, lua: &'_ mlua::Lua) -> mlua::Result<Self> {
+        todo!()
+    }
+}
+
+impl IntoLua<'_> for ContentPackage {
+    fn into_lua(self, lua: &'_ mlua::Lua) -> mlua::Result<mlua::Value<'_>> {
         todo!()
     }
 }
