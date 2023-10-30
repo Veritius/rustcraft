ab952964041afdba33fcc00a59d05cfabb6099f1
veritius <veritiusgaming@gmail.com>
2023-10-31 10:57:44 +1100

    big_ids feature flag

--- engine/src/blocks/id.rs
+++ engine/src/blocks/id.rs
@@ -1,4 +1,76 @@
-/// A unique, short numerical identifier for a block, created by a [`BlockRegistryBuilder`](super::registry::BlockRegistryBuilder).
-/// A `BlockId` is only valid when using the `BlockRegistry` that created it. Using it with another registry may return unexpected results or panic.
+/// A unique, short numerical identifier for a block, used for dense storage of voxel information.
+/// `BlockId`s are useless by themselves, and are used to access information stored in the [`BlockRegistry`](super::registry::BlockRegistry).
+/// 
+/// A `BlockId` normally has `65536` aka `2^16` possible values.
+/// With the `big_ids` feature flag, it has `16777216` aka `2^24` possible values.
+/// Accordingly, the size in memory goes from 2 bytes to 3 bytes.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-pub struct BlockId(pub(super) u16);
+pub struct BlockId(internal::BlockIdInner);
+
+#[cfg(not(feature="big_ids"))]
+mod internal {
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+    pub(super) struct BlockIdInner(u16);
+    
+    impl From<u16> for BlockIdInner {
+        fn from(value: u16) -> Self {
+            Self(value)
+        }
+    }
+    
+    impl Into<u16> for BlockIdInner {
+        fn into(self) -> u16 {
+            self.0
+        }
+    }
+}
+
+#[cfg(feature="big_ids")]
+mod internal {
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+    pub(super) struct BlockIdInner([u8;3]);
+
+    impl TryFrom<u32> for BlockIdInner {
+        type Error = ();
+
+        fn try_from(value: u32) -> Result<Self, Self::Error> {
+            if value > 2u32.pow(24) { return Err(() )}
+            let bytes = value.to_be_bytes();
+            Ok(Self([bytes[0], bytes[1], bytes[2]]))
+        }
+    }
+
+    impl Into<u32> for BlockIdInner {
+        fn into(self) -> u32 {
+            u32::from_be_bytes([self.0[0], self.0[1], self.0[2], 0])
+        }
+    }
+}
+
+/// Creates a sequential series of `BlockID`s.
+/// Panics when the upper limit of `BlockID`s are achieved.
+#[derive(Default)]
+pub(super) struct BlockIdGenerator {
+    #[cfg(not(feature="big_ids"))]
+    state: u16,
+    #[cfg(feature="big_ids")]
+    state: u32,
+}
+
+impl BlockIdGenerator {
+    #[cfg(not(feature="big_ids"))]
+    pub fn next(&mut self) -> BlockId {
+        let id = BlockId(internal::BlockIdInner::from(self.state));
+        self.state = self.state.checked_add(1)
+            .expect("Ran out of possible BlockId values at 2^16");
+        return id
+    }
+
+    #[cfg(feature="big_ids")]
+    pub fn next(&mut self) -> BlockId {
+        if self.state > 2u32.pow(24) { panic!("Ran out of possible BlockId values at 2^24") }
+        let id = BlockId(internal::BlockIdInner::try_from(self.state).unwrap());
+        self.state += 1;
+        return id
+    }
+}
--- engine/src/blocks/registry.rs
+++ engine/src/blocks/registry.rs
@@ -1,7 +1,7 @@
 use std::{sync::Arc, collections::BTreeMap, any::Any};
 use bevy::{prelude::*, utils::HashMap};
 use crate::content::id::ContentIdentifier;
-use super::id::BlockId;
+use super::id::{BlockId, BlockIdGenerator};
 
 /// A value that is added to a [BlockDefinition] in the [BlockRegistry] to define behaviors for a block..
 pub trait BlockAttribute: std::fmt::Debug + Send + Sync + Any {}
@@ -10,7 +10,7 @@ impl<T: std::fmt::Debug + Send + Sync + Any> BlockAttribute for T {}
 /// Used during the setup stage of the game to create the [BlockRegistry].
 #[derive(Resource)]
 pub struct BlockRegistryBuilder {
-    seq: u16,
+    seq: BlockIdGenerator,
     inner: BlockRegistryInner,
 }
 
@@ -22,8 +22,7 @@ impl BlockRegistryBuilder {
         ident: ContentIdentifier,
         attributes: impl Iterator<Item = (ContentIdentifier, Box<dyn BlockAttribute>)>
     ) {
-        let id = BlockId(self.seq);
-        self.seq += 1;
+        let id = self.seq.next();
         self.inner.identifiers.insert(ident, id)
             .expect("Registry already contained block {ident}");
         self.inner.definitions.insert(id, BlockDefinition {
