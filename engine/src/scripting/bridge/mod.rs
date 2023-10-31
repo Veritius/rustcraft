13561e821c0059ddfd61b0fe80f932e15753a15e
veritius <veritiusgaming@gmail.com>
2023-10-31 16:23:08 +1100

    Improve Bridge<T>

--- engine/src/scripting/bridge/mod.rs
+++ engine/src/scripting/bridge/mod.rs
@@ -1,9 +1,10 @@
 mod stdlib;
+mod bevy;
 
-/// A bridge between types from foreign crates and Lua.
+/// A bridge between types from foreign crates and Lua, to work around the orphan rules.
 /// Implements `Deref` for `T`, most standard library traits where `T` does, and `FromLua` and `IntoLua` for certain types.
 #[repr(transparent)]
-pub struct Bridge<T>(T);
+pub struct Bridge<T>(pub T);
 
 impl<T> std::ops::Deref for Bridge<T> {
     type Target = T;
@@ -16,5 +17,11 @@ impl<T> std::ops::Deref for Bridge<T> {
 impl<T> std::ops::DerefMut for Bridge<T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut self.0
+    }
+}
+
+impl<T> From<T> for Bridge<T> {
+    fn from(value: T) -> Self {
+        Self(value)
     }
 }
