a49e2ac12854eaa1566aae71b25382a2a46d8dd7
veritius <veritiusgaming@gmail.com>
2023-11-03 15:21:14 +1100

    add_alias fn

--- engine/src/blocks/registry.rs
+++ engine/src/blocks/registry.rs
@@ -12,7 +12,8 @@ pub struct BlockRegistryBuilder {
 
 impl BlockRegistryBuilder {
     /// Registers a block.
-    /// Panics if the block already exists
+    /// 
+    /// Panics if `ident` is already assigned.
     pub fn add_block(
         &mut self,
         ident: ContentIdentifier,
@@ -24,6 +25,20 @@ impl BlockRegistryBuilder {
         self.inner.definitions.insert(id, BlockDefinition {
             attributes: attributes.collect()
         });
+    }
+
+    /// Registers `alias` to refer to the same `BlockId` as `ident`.
+    /// 
+    /// If `ident` does not exist, this will silently fail.
+    /// This behavior may change in future.
+    pub fn add_alias(
+        &mut self,
+        ident: &ContentIdentifier,
+        alias: ContentIdentifier,
+    ) {
+        let id = self.inner.identifiers.get(ident);
+        if id.is_none() { return }
+        self.inner.identifiers.insert(alias, *id.unwrap());
     }
 
     /// Adds an attribute to an already-existing block.
